49a85824b2879dfeac88455c8dc8440073fdebdf
Heather Lamkins <lamkinsheather@gmail.com>
2020-10-13 21:46:14 -0500

    Added elements to js file

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+let lower = "abcdefghijklmnopqrstuvwxyz";
+let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let numbers = "0123456789"
+let symbols =  " `~!@#$%^&*()-_=+[]{}\|?/.<>'"
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+function generatePassword() {
+  let passLength = prompt("How many characters do should your password contain?")
+  let lowerCase = confirm("Do you want your password to include lowercase letters?")
+  let upperCase = confirm("Do you want your password to include uppercase letters?")
+  let numChoice = confirm("Do you want your password to include numbers?")
+  let symChoice = confirm("Do you want your password to include symbols?")
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+  

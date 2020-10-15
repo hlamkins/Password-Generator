4602f4e54a76b963f3bf09ab51c21b34bd1fbd43
Heather Lamkins <lamkinsheather@gmail.com>
2020-10-15 15:51:27 -0500

    Added boolean statements to javascript

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let generateBtn = document.querySelector("#generate");
 let lower = "abcdefghijklmnopqrstuvwxyz";
 let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 let numbers = "0123456789"
-let symbols =  " `~!@#$%^&*()-_=+[]{}\|?/.<>'"
+let specChar = " !#$@%^&*(){}?/\~`|"
+
 
 // Write password to the #password input
 function writePassword() {
@@ -15,14 +16,34 @@ function writePassword() {
 }
 
 function generatePassword() {
-  let passLength = prompt("How many characters do should your password contain?")
-  let lowerCase = confirm("Do you want your password to include lowercase letters?")
-  let upperCase = confirm("Do you want your password to include uppercase letters?")
-  let numChoice = confirm("Do you want your password to include numbers?")
-  let symChoice = confirm("Do you want your password to include symbols?")
+
+let passLength = (prompt)("How many characters should your password contain?"));
+
+let lowerCase = confirm("Do you want your password to include lowercase letters?");
+let upperCase = confirm("Do you want your password to include uppercase letters?");
+let numChoice = confirm("Do you want your password to include numbers?");
+let symChoice = confirm("Do you want your password to include symbols?");
+
+let eligibleChars = ""
+
+if (lowerCase === true) {
+  eligibleChars = eligibleChars + lower;
+if (upperCase === true) {
+  eligibleChars = eligibleChars + upper;
+} 
+if (numChoice === true) {
+  eligibleChars = eligibleChars + numbers;
 }
+if (symChoice === true) {
+  eligibleChars = eligibleChars + specChar;
+}
+
+
+
+
+
+
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-  

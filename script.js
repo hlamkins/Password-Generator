935a57ee912563334a2374263d695979673dbe53
Heather Lamkins <lamkinsheather@gmail.com>
2020-10-15 16:06:26 -0500

    Non-working but mostly written code

--- script.js
+++ script.js
@@ -18,13 +18,19 @@ function writePassword() {
 function generatePassword() {
 
 let passLength = (prompt)("How many characters should your password contain?"));
+ if (passLength >= 8 && <=128) {
+
+ }
+ else (alert("Password must be between 8 and 128 characters.")){
+  XMLHttpRequestUpload()
+ }
 
 let lowerCase = confirm("Do you want your password to include lowercase letters?");
 let upperCase = confirm("Do you want your password to include uppercase letters?");
 let numChoice = confirm("Do you want your password to include numbers?");
 let symChoice = confirm("Do you want your password to include symbols?");
 
-let eligibleChars = ""
+let eligibleChars = "";
 
 if (lowerCase === true) {
   eligibleChars = eligibleChars + lower;
@@ -38,6 +44,16 @@ if (symChoice === true) {
   eligibleChars = eligibleChars + specChar;
 }
 
+function password(l, eligibleChars){
+var pwd = "";
+for(var i = 0, i<l; i++){
+  pwd += eligibleChars.charAt(Math.floor(Math.random() * characters.length));
+
+}
+
+
+
+}
 
 
 
